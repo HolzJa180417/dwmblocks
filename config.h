2dc4dfc590970f289cbef5447df6b37fedde589b
HolzJa180417 <40146720180417@litec.ac.at>
2022-10-20 16:24:58 +0200

    update

--- config.h
+++ config.h
@@ -14,15 +14,16 @@ static const Block blocks[] = {
 	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"sb-torrent",	20,	7}, */
-	{"",	"sb-memory",	10,	14}, 
-	{"",	"sb-cpu",		10,	18},
+	{"",	"sb-memory",	10,	14},
+        {"",	"sb-cpuutil",	1,	16},	
+	{"",	"sb-cputemp",	10,	18},
 	{"",	"sb-disk",	18000,	17},
 	{"",	"sb-forecast",	18000,	5},
 	/* {"",	"sb-mailbox",	180,	12}, */
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	2,	10},
 	{"",	"sb-battery",	5,	3},
-	{"",	"sb-clock",	60,	1},
+	{"",	"sb-clock",	30,	1},
 	/* ,	"sb-internet",	5,	4}, */
 	/* ,	"sb-help-icon",	0,	15}, */
 };
